c0e52d42d0d8fa26056f978667a19182b7cdb4eb
Owen Duffy <oduffy1@gmail.com>
2024-12-06 21:08:35 -0600

    wefr

---
+++ request.py
@@ -0,0 +1,77 @@
+import requests
+import os
+from dotenv import find_dotenv, load_dotenv
+
+location = find_dotenv()
+load_dotenv(location)
+
+
+url = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29"
+
+body = {
+	"parameters": {
+		"decoding_method": "sample",
+		"max_new_tokens": 200,
+		"min_new_tokens": 50,
+		"random_seed": 111,
+		"temperature": 1.14,
+		"top_k": 50,
+		"top_p": 1,
+		"repetition_penalty": 2
+	},
+	"model_id": "google/flan-t5-xxl",
+	"project_id": os.getenv('project_id'),
+	"moderations": {
+		"hap": {
+			"input": {
+				"enabled": True,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": True
+				}
+			},
+			"output": {
+				"enabled": True,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": True
+				}
+			}
+		},
+		"pii": {
+			"input": {
+				"enabled": True,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": True
+				}
+			},
+			"output": {
+				"enabled": True,
+				"threshold": 0.5,
+				"mask": {
+					"remove_entity_value": True
+				}
+			}
+		}
+	}
+}
+
+headers = {
+	"Accept": "application/json",
+	"Content-Type": "application/json",
+	"Authorization": "Bearer " + os.getenv('api_key')
+}
+
+response = requests.post(
+	url,
+	headers=headers,
+	json=body
+)
+
+#if response.status_code != 200:
+#	raise Exception("Non-200 response: " + str(response.text))
+
+data = response.json()
+
+print(data)
